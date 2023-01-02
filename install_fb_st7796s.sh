c9ce2170dfb118ba1395444ce5ad595c6011197a
Eugene Stahov <eugene.stahov@gmail.com>
2023-01-02 18:27:25 +0500

    Create install_fb_st7796s.sh

---
+++ install_fb_st7796s.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+echo "Check kernel architecture..."
+uname -a | grep sunxi64 || die "Unknown kernel architecture"
+
+sudo apt update
+sudo apt install git build-essential linux-headers-current-sunxi64
+
+cd ~
+rm -rf fb_st7796s
+
+echo "Fetching sources..."
+git clone https://github.com/Sergey1560/fb_st7796s.git
+cd ~/fb_st7796s/kernel_module/
+
+echo "Building driver..."
+make
+
+echo "Installing kernel module..."
+sudo make install
+make clean
+sudo depmod -A
+
+echo "Appending to initramfs..."
+
+grep -qxF 'fb_st7796s' || echo fb_st7796s | sudo tee /etc/initramfs-tools/modules
+sudo update-initramfs -u
+
+echo "Installing overlay..."
+sudo armbian-add-overlay ~/fb7796s/dts/sun50i-h6-st7796s.dts
+
+echo "Your need reboot your SBC to activate module"
