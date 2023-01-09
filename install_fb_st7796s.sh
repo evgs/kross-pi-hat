e3aacee1dbd492017d2ad23b6341c9f4d1d04586
Eugene Stahov <eugene.stahov@gmail.com>
2023-01-09 19:23:09 +0500

    Update install_fb_st7796s.sh

--- install_fb_st7796s.sh
+++ install_fb_st7796s.sh
@@ -11,17 +11,17 @@ echo "Check kernel architecture..."
 uname -a | grep sunxi64 || die "Unknown kernel architecture"
 
 sudo apt update
-sudo apt install git build-essential linux-headers-current-sunxi64
+sudo apt install git build-essential linux-headers-current-sunxi64 || die( "Error while installing packages")
 
 cd ~
 rm -rf fb_st7796s
 
 echo "Fetching sources..."
-git clone $SOURCES
+git clone $SOURCES || die( "Error while fetching sources from github")
 cd ~/fb_st7796s/kernel_module/
 
 echo "Building driver..."
-make
+make  || die ("Driver compiling fault")
 
 echo "Installing kernel module..."
 sudo make install
@@ -36,8 +36,14 @@ sudo update-initramfs -u
 echo "Installing overlay..."
 sudo armbian-add-overlay ~/fb_st7796s/dts/sun50i-h6-st7796s.dts
 
+sudo systemctl stop KlipperScreen.service
+sudo rm /etc/X11/xorg.conf.d/50-fbturbo.conf
+sudo apt remove xserver-xorg-video-fbturbo
+sudo apt install xserver-xorg-video-fbdev
+
 echo "Copying xorg.conf rules..."
 sudo cp ~/fb_st7796s/xorg.conf.d/50* /etc/X11/xorg.conf.d
 sudo cp ~/fb_st7796s/xorg.conf.d/51* /etc/X11/xorg.conf.d
+sudo cp ~/fb_st7796s/X11/Xwrapper.conf /etc/X11/
 
 echo "Your need reboot your SBC to activate module"
